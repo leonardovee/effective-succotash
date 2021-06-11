227d9f165c36f4be2dd33b9a3b80a36a356e2f18
leonardovee <leoveeph@gmail.com>
2021-06-11 20:38:40 -0300

    feat: ensure AddUserRepository creates user on success

--- app/Infra/Db/MySql/User/AddUserRepository.php
+++ app/Infra/Db/MySql/User/AddUserRepository.php
@@ -2,7 +2,15 @@
 
 namespace App\Infra\Db\MySql\User;
 
+use App\Models\User;
+
 class AddUserRepository
 {
-    public function add () {}
+    public function add (array $userData)
+    {
+        $user = new User($userData);
+        $user->save();
+
+        return $user->id;
+    }
 }
