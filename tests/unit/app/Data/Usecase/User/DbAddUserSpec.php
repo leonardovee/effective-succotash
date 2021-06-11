937c1c167bd290d50d76a6f0885a1dfdd6d00b39
leonardovee <leoveeph@gmail.com>
2021-06-11 20:15:32 -0300

    test: ensure DbAddUser calls AddUserRepository with correct values

---
+++ tests/unit/app/Data/Usecase/User/DbAddUserSpec.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Data\Usecase\User\DbAddUser;
+use App\Infra\Db\MySql\User\AddUserRepository;
+use Illuminate\Http\Request;
+
+class DbAddUserTest extends TestCase
+{
+    private $sut;
+    private $stub;
+
+    private function makeRequest(): array
+    {
+        return [
+            'name' => 'any_name',
+            'email' => 'any_email',
+            'password' => 'any_password',
+            'publicRegistry' => 'any_public_registry',
+            'type' => 1
+        ];
+    }
+
+    private function makeSut()
+    {
+        $this->stub = $this->createMock(AddUserRepository::class);
+
+        $this->sut = new DbAddUser($this->stub);
+    }
+
+    public function test_should_call_add_user_repository_with_correct_values()
+    {
+        $this->makeSut();
+
+        $request = $this->makeRequest();
+
+        $this->stub->expects($this->once())->method('add')->with($request);
+
+        $this->sut->add($request);
+    }
+}
