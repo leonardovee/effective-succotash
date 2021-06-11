5fef1fc201d4086e25e472be9c5a6b628d269c6f
leonardovee <leoveeph@gmail.com>
2021-06-11 20:38:48 -0300

    test: ensure AddUserRepository creates user on success

---
+++ tests/unit/app/Infra/Db/MySql/User/AddUserRepositorySpec.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Infra\Db\MySql\User\AddUserRepository;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+
+class AddUserRepositoryTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    private $sut;
+    private $stub;
+
+    private function makeUserData(): array
+    {
+        return [
+            'name' => 'any_name',
+            'email' => 'any_email',
+            'password' => 'any_password',
+            'document' => 'any_document',
+            'type' => 1
+        ];
+    }
+
+    private function makeSut()
+    {
+        $this->sut = new AddUserRepository($this->stub);
+    }
+
+    public function test_should_add_a_user_on_success()
+    {
+        $this->makeSut();
+
+        $request = $this->makeUserData();
+
+        $this->sut->add($request);
+
+        $this->seeInDatabase('user', $request);
+    }
+}
