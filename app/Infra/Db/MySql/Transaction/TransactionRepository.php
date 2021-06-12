5d3d8016a6782daedf197a0d9687ceded934f302
leonardovee <leoveeph@gmail.com>
2021-06-12 16:42:39 -0300

    feat: ensure TransactionRepository creates a transaction

---
+++ app/Infra/Db/MySql/Transaction/TransactionRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Infra\Db\MySql\Transaction;
+
+use App\Data\Protocols\Db\MySql\Transaction\AddTransactionRepository;
+use App\Domain\Model\Deposit;
+use App\Domain\Model\Transaction;
+use App\Domain\Model\Withdraw;
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Throwable;
+
+class TransactionRepository implements AddTransactionRepository
+{
+    public function add (Deposit $depositData, Withdraw $withdrawData) : Transaction
+    {
+        try {
+            DB::beginTransaction();
+
+            $deposit = new \App\Models\Deposit();
+            $deposit->amount = $depositData->amount;
+            $deposit->user = $depositData->user;
+            $deposit->save();
+
+            $withdraw = new \App\Models\Withdraw();
+            $withdraw->amount = $withdrawData->amount;
+            $withdraw->user = $withdrawData->user;
+            $withdraw->save();
+
+            $transaction = new \App\Models\Transaction();
+            $transaction->deposit = $deposit->id;
+            $transaction->withdraw = $withdraw->id;
+            $transaction->save();
+
+            DB::commit();
+        } catch (Throwable $error) {
+            DB::rollBack();
+            throw new Exception();
+        }
+
+        $result = new Transaction();
+        $result->id = $transaction->id;
+        $result->deposit = $deposit;
+        $result->withdraw = $withdraw;
+
+        return $result;
+    }
+}
---
+++ app/Models/Transaction.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $table = 'transaction';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'deposit', 'withdraw'
+    ];
+}
