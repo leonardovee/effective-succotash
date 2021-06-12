46c0c308dbf67791229c4dba0ed510c15cc670d2
leonardovee <leoveeph@gmail.com>
2021-06-12 17:23:23 -0300

    feat: ensure TransactionController calls DbCreateTransaction with correct values

--- app/Http/Controllers/Transaction/TransactionController.php
+++ app/Http/Controllers/Transaction/TransactionController.php
@@ -2,17 +2,34 @@
 
 namespace App\Http\Controllers\Transaction;
 
+use App\Domain\Model\Deposit;
+use App\Domain\Model\Withdraw;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
+    public function __construct($dbCreateTransaction)
+    {
+        $this->dbCreateTransaction = $dbCreateTransaction;
+    }
+
     public function handle(Request $request)
     {
         $this->validate($request, [
             'payer' => 'required|int:user',
             'payee' => 'required|int:user',
-            'amount' => 'required|int'
+            'amount' => 'required|numeric'
         ]);
+
+        $deposit = new Deposit();
+        $deposit->user = $request->payee;
+        $deposit->amount = $request->amount;
+
+        $withdraw = new Withdraw();
+        $withdraw->user = $request->payer;
+        $withdraw->amount = $request->amount;
+
+        $this->dbCreateTransaction->create($deposit, $withdraw);
     }
 }
