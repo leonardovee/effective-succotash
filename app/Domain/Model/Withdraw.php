e09a556372b8d693162e7e879dfefd82a18fd588
leonardovee <leoveeph@gmail.com>
2021-06-12 18:30:33 -0300

    feat: create factories for Deposit and Withdraw models

--- app/Domain/Model/Withdraw.php
+++ app/Domain/Model/Withdraw.php
@@ -3,6 +3,13 @@
 namespace App\Domain\Model;
 
 class Withdraw {
+    public function __construct(int $id = 0, string $user = '', int $amount = 0)
+    {
+        $this->id = $id;
+        $this->user = $user;
+        $this->amount = $amount;
+    }
+
     public int $id;
     public string $user;
     public int $amount;
