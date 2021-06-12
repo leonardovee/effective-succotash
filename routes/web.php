c5108e53bc04ae5851a12816c66d7d33203278a6
leonardovee <leoveeph@gmail.com>
2021-06-11 21:20:01 -0300

    feat: compose user post route

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
-/** @var \Laravel\Lumen\Routing\Router $router */
+use App\Factories\Controllers\AddUserControllerFactory;
+use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->post('/user', function (Request $request) {
+    $addUserController = AddUserControllerFactory::make();
+    return $addUserController->handle($request);
 });
