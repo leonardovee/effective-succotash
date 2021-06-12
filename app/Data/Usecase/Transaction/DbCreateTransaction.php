5ce1e5a695c0c1d5247505211171861561e0b086
leonardovee <leoveeph@gmail.com>
2021-06-11 23:55:32 -0300

    feat: ensure DbCreateTransaction calls GetDepositsRepository with correct values

--- app/Data/Usecase/Transaction/DbCreateTransaction.php
+++ app/Data/Usecase/Transaction/DbCreateTransaction.php
@@ -9,14 +9,16 @@ use App\Domain\Usecase\Transaction\CreateTransaction;
 
 class DbCreateTransaction implements CreateTransaction
 {
-    public function __construct($getWithdrawsRepository)
+    public function __construct($getWithdrawsRepository, $getDepositsRepository)
     {
         $this->getWithdrawsRepository = $getWithdrawsRepository;
+        $this->getDepositsRepository = $getDepositsRepository;
     }
 
     public function create (Deposit $deposit, Withdraw $withdraw): Transaction
     {
         $this->getWithdrawsRepository->get($withdraw->user);
+        $this->getDepositsRepository->get($withdraw->user);
         return new Transaction();
     }
 }
