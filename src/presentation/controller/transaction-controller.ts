23c02439836a3f6dddf7b5769a2faebd1e399041
leonardovee <leoveeph@gmail.com>
2021-12-24 13:14:49 -0300

    feat: ensure TransactionController returns 422 if no amount is provided

--- src/presentation/controller/transaction-controller.ts
+++ src/presentation/controller/transaction-controller.ts
@@ -5,12 +5,15 @@ import { MissingParamError } from '@/presentation/error/missing-param-error'
 
 export class TransactionController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { payer, payee } = httpRequest.body
+    const { payer, payee, amount } = httpRequest.body
     if (!payer) {
       return unprocessableEntity(new MissingParamError('payer'))
     }
     if (!payee) {
       return unprocessableEntity(new MissingParamError('payee'))
+    }
+    if (!amount) {
+      return unprocessableEntity(new MissingParamError('amount'))
     }
   }
 }
