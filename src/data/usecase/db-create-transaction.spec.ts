2ad93d67fbca67bfd1b40e92c1ecfcad439a6bb9
leonardovee <leoveeph@gmail.com>
2021-12-23 23:08:29 -0300

    test: ensure DbCreateTransaction throws if payer is bussiness

--- src/data/usecase/db-create-transaction.spec.ts
+++ src/data/usecase/db-create-transaction.spec.ts
@@ -1,13 +1,15 @@
 import { DbCreateTransaction } from '@/data/usecase/db-create-transaction'
 import { UserModel } from '@/domain/model/user'
 import { LoadUserByIdRepository } from '@/data/protocol/load-user-by-id-repository'
+import { UnauthorizedTransactionError } from '@/error/unauthorized-transaction-error'
 
 const makeFakeUser = (): UserModel => {
   return {
-    id: "any_id",
-    name: "any_name",
-    email: "any_email",
-    password: "any_password"
+    id: 'any_id',
+    name: 'any_name',
+    type: 'user',
+    email: 'any_email',
+    password: 'any_password'
   }
 }
 
@@ -25,13 +27,35 @@ describe('DbCreateTransaction', () => {
     const loadUserByIdRepositoryStub = makeLoadUserByIdRepository()
     const sut = new DbCreateTransaction(loadUserByIdRepositoryStub)
     const loadByIdSpy = jest.spyOn(loadUserByIdRepositoryStub, 'loadById')
-    sut.create({
-      user: "any_user",
+    await sut.create({
+      user: 'any_user',
       amount: 1000
     }, {
-      user: "other_user",
+      user: 'other_user',
       amount: 1000
     })
-    expect(loadByIdSpy).toHaveBeenCalledWith("other_user")
+    expect(loadByIdSpy).toHaveBeenCalledWith('other_user')
+  })
+
+  it('Should throw unauthorized transaction error if payer is bussiness type', async () => {
+    const loadUserByIdRepositoryStub = makeLoadUserByIdRepository()
+    const sut = new DbCreateTransaction(loadUserByIdRepositoryStub)
+    jest.spyOn(loadUserByIdRepositoryStub, 'loadById').mockReturnValueOnce(
+      new Promise(resolve => resolve({
+        id: 'any_id',
+        name: 'any_name',
+        type: 'bussiness',
+        email: 'any_email',
+        password: 'any_password'
+      }))
+    )
+    const promise = sut.create({
+      user: 'any_user',
+      amount: 1000
+    }, {
+      user: 'other_user',
+      amount: 1000
+    })
+    await expect(promise).rejects.toThrow(UnauthorizedTransactionError)
   })
 })
