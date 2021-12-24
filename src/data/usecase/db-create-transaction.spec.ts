f990b7074be46a12e03b30d8163a65e7a17d53f2
leonardovee <leoveeph@gmail.com>
2021-12-23 23:16:21 -0300

    refactor: move sut creation to a helper method

--- src/data/usecase/db-create-transaction.spec.ts
+++ src/data/usecase/db-create-transaction.spec.ts
@@ -2,6 +2,18 @@ import { DbCreateTransaction } from '@/data/usecase/db-create-transaction'
 import { UserModel } from '@/domain/model/user'
 import { LoadUserByIdRepository } from '@/data/protocol/load-user-by-id-repository'
 import { UnauthorizedTransactionError } from '@/error/unauthorized-transaction-error'
+import { DepositModel } from '@/domain/model/deposit'
+import { WithdrawModel } from '@/domain/model/withdraw'
+
+const makeFakeRequest = (): [DepositModel, WithdrawModel] => {
+  return [{
+    user: 'any_user',
+    amount: 1000
+  }, {
+    user: 'other_user',
+    amount: 1000
+  }]
+}
 
 const makeFakeUser = (): UserModel => {
   return {
@@ -22,24 +34,32 @@ const makeLoadUserByIdRepository = (): LoadUserByIdRepository => {
   return new LoadUserByIdRepositoryStub()
 }
 
+interface SutTypes {
+  sut: DbCreateTransaction,
+  loadUserByIdRepositoryStub: LoadUserByIdRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadUserByIdRepositoryStub = makeLoadUserByIdRepository()
+  const sut = new DbCreateTransaction(loadUserByIdRepositoryStub)
+  return {
+    sut,
+    loadUserByIdRepositoryStub
+  }
+}
+
 describe('DbCreateTransaction', () => {
   it('Should call user repository with correct values', async () => {
-    const loadUserByIdRepositoryStub = makeLoadUserByIdRepository()
-    const sut = new DbCreateTransaction(loadUserByIdRepositoryStub)
+    const { sut, loadUserByIdRepositoryStub } = makeSut()
     const loadByIdSpy = jest.spyOn(loadUserByIdRepositoryStub, 'loadById')
-    await sut.create({
-      user: 'any_user',
-      amount: 1000
-    }, {
-      user: 'other_user',
-      amount: 1000
-    })
+
+    await sut.create(...makeFakeRequest())
+
     expect(loadByIdSpy).toHaveBeenCalledWith('other_user')
   })
 
   it('Should throw unauthorized transaction error if payer is bussiness type', async () => {
-    const loadUserByIdRepositoryStub = makeLoadUserByIdRepository()
-    const sut = new DbCreateTransaction(loadUserByIdRepositoryStub)
+    const { sut, loadUserByIdRepositoryStub } = makeSut()
     jest.spyOn(loadUserByIdRepositoryStub, 'loadById').mockReturnValueOnce(
       new Promise(resolve => resolve({
         id: 'any_id',
@@ -49,13 +69,9 @@ describe('DbCreateTransaction', () => {
         password: 'any_password'
       }))
     )
-    const promise = sut.create({
-      user: 'any_user',
-      amount: 1000
-    }, {
-      user: 'other_user',
-      amount: 1000
-    })
+
+    const promise = sut.create(...makeFakeRequest())
+
     await expect(promise).rejects.toThrow(UnauthorizedTransactionError)
   })
 })
