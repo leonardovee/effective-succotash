4c4fe9858742c18268210297ed61a1ea42aacaf6
leonardovee <leoveeph@gmail.com>
2021-12-24 13:48:40 -0300

    fix: model

--- src/domain/usecase/create-transaction.ts
+++ src/domain/usecase/create-transaction.ts
@@ -1,6 +1,6 @@
-import { Deposit } from '@/domain/model/deposit'
-import { Withdraw } from '@/domain/model/withdraw'
+import { DepositModel } from '@/domain/model/deposit'
+import { WithdrawModel } from '@/domain/model/withdraw'
 
 export interface CreateTransaction {
-  create: (deposit: Deposit, withdraw: Withdraw) => Promise<TransactionModel>
+  create: (deposit: DepositModel, withdraw: WithdrawModel) => Promise<TransactionModel>
 }
