bef52d6abd28d0922df4a37fed7a54d3e5e940fd
leonardovee <leoveeph@gmail.com>
2021-12-24 15:35:50 -0300

    feat: ensure MongoHelper connect to instance of mongo

---
+++ src/infra/db/mongodb/helper/mongo-helper.ts
@@ -0,0 +1,28 @@
+import { MongoClient, Collection } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as unknown as MongoClient,
+  url: null as unknown as string,
+
+  async connect (url: string): Promise<void> {
+    this.url = url
+    this.client = await MongoClient.connect(url)
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client.close()
+    this.client = null as unknown as MongoClient
+  },
+
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client) {
+      await this.connect(this.url)
+    }
+    return this.client.db().collection(name)
+  },
+
+  map (collection: any): any {
+    const { _id, ...collectionWithoudId } = collection
+    return Object.assign({}, collectionWithoudId, { id: _id })
+  }
+}
